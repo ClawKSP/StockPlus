089c9b3f150a7a236b9f4e6e5463fb3cda3f3282
ClawKSP <ClawKSP@gmail.com>
2015-05-04 21:32:00 -1000

    v0.0.1
    v0.0.1 (4 May 15) - Initial release, includes
    ModuleProceduralFairingPlus

---
+++ README StockPlus.txt
@@ -0,0 +1,40 @@
+KSP-StockPlus
+=========================
+
+Small additions to the game.
+
+
+Installation
+============
+Unzip the zip file to the KSP/GameData directory. ModuleManager.X.X.X.dll should be in the root GameData directory next to a StockPlus folder.
+
+The additions are all stand alone. So if there are any that you don't want, just delete the directory inside the StockPlus folder.
+
+
+
+Additions Included
+==================
+
++ ModuleProceduralFairingPlus
+Description: Fixes and tweakable options for fairings
+  - Fixes some bugs with removing/replacing interstage fairings which can cause fairings to lock up
+  - Fixes fairing decoupler force (small fairings are too high, and large fairings too low)
+  - Adds tweakable that allows adjusting the number of shell splits on the fairing
+  - Adds tweakable decoupler force range.
+  - Adds tweakable option to enable/disable fairings inside a procedural fairing
+
+--
+
+
+License
+=======
+
+Covered under the CC-BY-NC-SA license. See the license.txt for more details.
+(https://creativecommons.org/licenses/by-nc-sa/4.0/)
+
+ModuleManager by Sarbian (bundled) is covered under a CC share-alike license.
+
+
+Change Log
+==========
+v0.0.1 (4 May 15) - Initial release, includes ModuleProceduralFairingPlus
---
+++ Source/ModuleJettisonFix.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * This module written by Claw. For more details, please visit
+ * http://forum.kerbalspaceprogram.com/threads/97285
+ * 
+ * This mod is covered under the CC-BY-NC-SA license. See the readme.txt for more details.
+ * (https://creativecommons.org/licenses/by-nc-sa/4.0/)
+ * 
+ *
+ * ModuleJettisonFix - Written for KSP v1.00
+ * 
+ * - Reactivates jettison shrouds for parts inside of aero shells and cargo bays.
+ *   When shrouds are missing, some parts appear to float.
+ * 
+ * Change Log:
+ * - v01.00 (26 Apr 15) Initial Release
+ * 
+ */
+
+using UnityEngine;
+using KSP;
+
+namespace ClawKSP
+{
+
+    public class MJFix : PartModule
+    {
+
+        // Insert Tweakable
+        [KSPField(guiName = "Fairing", isPersistant = true, guiActiveEditor = true)]
+        [UI_Toggle(enabledText = "Removed", disabledText = "Normal", scene = UI_Scene.Editor)]
+        public bool isJettisoned = false;
+
+        [KSPField(isPersistant = true)]
+        private bool noLongerShielded = false;
+
+        ModuleJettison JettisonModule;
+
+        public void Start ()
+        {
+            JettisonModule = (ModuleJettison) part.Modules["ModuleJettison"];
+            Debug.Log("MJFix.Start(): v01.00");
+        }
+
+        public void LateUpdate()
+        {
+            if (HighLogic.LoadedScene == GameScenes.EDITOR || HighLogic.LoadedScene == GameScenes.FLIGHT)
+            {
+                if (part.ShieldedFromAirstream == true)
+                {
+                    Fields["isJettisoned"].guiActiveEditor = true;
+                    JettisonModule.isJettisoned = isJettisoned;
+                    JettisonModule.jettisonTransform.gameObject.SetActive(!isJettisoned);
+                    
+                }
+                else
+                {
+                    Fields["isJettisoned"].guiActiveEditor = false;
+                }
+            }
+            else if (HighLogic.LoadedScene == GameScenes.FLIGHT)
+            {
+                Fields["isJettisoned"].guiActiveEditor = false;
+                if (part.ShieldedFromAirstream == true && noLongerShielded == false)
+                {
+                    JettisonModule.isJettisoned = isJettisoned;
+                    JettisonModule.jettisonTransform.gameObject.SetActive(!isJettisoned);
+                }
+                else
+                {
+                    noLongerShielded = true;
+                }
+            }
+        }
+
+    }
+}
